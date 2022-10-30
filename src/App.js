a0a54dbb3a087114ca0cd965b615d4d2f57f13b1
mattiamorosi <66827085+mattiamorosi@users.noreply.github.com>
2022-10-30 13:36:14 +0100

    Better files organization

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Onboarding from './screens/Onboarding';
-import GetCred from './screens/GetCred';
-import VerifyCred from './screens/VerifyCred';
+import Onboarding from './screens/Onboarding/Onboarding';
+import GetCred from './screens/GetCred/GetCred';
+import VerifyCred from './screens/VerifyCred/VerifyCred';
 
 const App = () => {
   const [alias, setAlias] = useState();
